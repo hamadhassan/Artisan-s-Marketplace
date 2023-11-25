326d78c6a3bbbd8a8108b43c6f21079a46bb05f8
Aleeza Shakeel <aleezashakeel55@gmail.com>
2023-11-25 17:35:03 +0500

    frontened_backened_attached

--- Backened/server.js
+++ Backened/server.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 require("./utils/db");
-port = 3005;
 
+const port = 3002;
 const app = express();
 
 app.use(bodyParser.json());
 app.use("/api", userRoutes);
 
+const staticPath = path.join(__dirname, "../public");
+app.use(express.static(staticPath));
+
 app.get("/", (req, res) => {
-  res.send("Welcome Aleeza");
+    res.sendFile(path.join(staticPath, "index.html"));
 });
- 
+
 app.listen(port, () => {
-  console.log(`Server is runing at port:${port}`);
+    console.log(`Server is running at port:${port}`);
 });
